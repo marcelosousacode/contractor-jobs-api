a5da5625cf34225e781fa62c06ea3f4bf798d024
Marcelo Sousa <marcelosousacode@gmail.com>
2021-07-10 14:10:00 -0300

    Fix CORS issue

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Clear.Rod22',
+  password: 'clear2022',
   database: 'bicos'
 })
 
