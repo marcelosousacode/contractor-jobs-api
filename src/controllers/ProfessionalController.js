43fdbed385e6e4f0b4c7e37042c7ba51bde318bd
Marcelo Sousa <marcelosousacode@gmail.com>
2021-08-02 14:10:00 -0300

    Merge pull request #4 from marceloribeiro/ProfessionalController
    Professional controller

--- src/controllers/ProfessionalController.js
+++ src/controllers/ProfessionalController.js
@@ -1,17 +1,16 @@
 const connection = require('../db/connection');
 
 module.exports = {
-
     async index(req, res) {
-        await connection.query('SELECT * FROM client', (err, rows) => {
-            if (err) throw err
+        await connection.query('SELECT * FROM professional', (err, rows) => {
+            if(err) throw err
             return res.json(rows)
         })
     },
 
     async show(req, res) {
         const id = req.params.id;
-        await connection.query('SELECT * FROM client WHERE client.id=?', [
+        await connection.query('SELECT * FROM professional WHERE professional.id=?', [
             id
         ], (err, rows) => {
             if (err) throw err
@@ -21,15 +20,15 @@ module.exports = {
 
     async create(req, res) {
         const { name, email, phone_number, uf, city, password } = req.body;
-        await connection.query('SELECT client.email FROM client WHERE client.email=?', [
+        await connection.query('SELECT professional.email FROM professional WHERE email=?', [
             email
         ], (err, rows) => {
-            if (err) throw err
-            if(rows[0]) {
+            if(err) throw err
+            if(rows[0]){
                 return res.json({ error: "E-mail already registered!" })
             }
 
-            connection.query('INSERT INTO client (name, email, phone_number, uf, city, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [
+            connection.query('INSERT INTO professional (name, email, phone_number, uf, city, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [
                 name,
                 email,
                 phone_number,
@@ -47,20 +46,23 @@ module.exports = {
                 return res.json(rows);
             })
         })
-        
     },
 
     async update(req, res) {
         const id = req.params.id;
-        const { name, email, cpf, phone_number, uf, city, password } = req.body;
-        await connection.query('UPDATE client SET name=?, email=?, cpf=?, phone_number=?, uf=?, city=?, password=?, updated_at=? WHERE client.id=?', [
+        const { name, email, cpf, phone_number, photo, uf, city, password, rate, description } = req.body;
+
+        await connection.query('UPDATE professional SET name=?, email=?, cpf=?, phone_number=?, photo=?, uf=?, city=?, password=?, rate=?, description=?, updated_at=? WHERE professional.id=?', [
             name,
             email,
             cpf,
             phone_number,
+            photo,
             uf,
             city,
             password,
+            rate, 
+            description,
             new Date().toISOString()
                 .replace(/T/, ' ')
                 .replace(/\..+/, ''),
@@ -73,14 +75,12 @@ module.exports = {
 
     async delete(req, res) {
         const id = req.params.id;
-        console.log(id)
-        await connection.query('DELETE FROM client where id = ?', 
+        await connection.query('DELETE FROM professional where id = ?', 
         [
             id
-        ], (err, rows, fields) => {
+        ], (err, rows) => {
             if (err) throw err
             return res.json(rows)
         })
     }
-
 }
