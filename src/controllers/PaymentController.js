e8a3d8562905c8728e81a1b425afb34c276eeeb3
Marcelo Sousa <marcelosousacode@gmail.com>
2021-09-22 14:10:00 -0300

    Handle Payment Controller errors

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -4,46 +4,71 @@ const connection = require('../db/connection');
 module.exports = {
     async createPaymentIntent(req, res) {
         const { items } = req.body;
-    
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: items.amount,
-            currency: "brl",
-            payment_method_types: ['card'],
-        });
-    
-        res.send({
-            clientSecret: paymentIntent.client_secret,
-            id: paymentIntent.id
-        });
+
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: items.amount,
+                currency: "brl",
+                payment_method_types: ['card'],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+                id: paymentIntent.id
+            });
+        } catch (error) {
+            res.send({
+                status: error.statusCode,
+                type: error.type,
+                message: error.raw.message
+            });
+        }
     },
     async confirmPaymentIntent(req, res) {
         const { paymentIntentId, paymentMethod } = req.body;
-    
-        const confirm = await stripe.paymentIntents.confirm(
-            paymentIntentId,
-            { payment_method: paymentMethod }
-        );
-    
-        res.send({
-            confirm
-        });
+        let confirm;
+
+        try {
+            confirm = await stripe.paymentIntents.confirm(
+                paymentIntentId,
+                { payment_method: paymentMethod }
+            );
+
+            res.send({
+                confirm
+            });
+        } catch (error) {
+            res.send({
+                status: error.statusCode,
+                type: error.type,
+                message: error.raw.message
+            });
+        }
     },
     async createPaymentMethod(req, res) {
         const { type, card } = req.body;
-        
-        const paymentMethod = await stripe.paymentMethods.create({
-            type: type,
-            card: {
-                number: card.number,
-                exp_month: card.exp_month,
-                exp_year: card.exp_year,
-                cvc: card.cvc,
-            },
-        });
-    
-        res.send({
-            paymentMethod: paymentMethod.id
-        })
+
+        try {
+            const paymentMethod = await stripe.paymentMethods.create({
+                type: type,
+                card: {
+                    number: card.number,
+                    exp_month: card.exp_month,
+                    exp_year: card.exp_year,
+                    cvc: card.cvc,
+                },
+            });
+
+            res.send({
+                paymentMethod: paymentMethod.id
+            })
+        } catch (error) {
+            res.send({
+                status: error.statusCode,
+                type: error.type,
+                message: error.raw.message
+            });
+        }
     },
     async savePayment(req, res) {
         const { clientId, payment } = req.body;
@@ -63,7 +88,7 @@ module.exports = {
             payment.code,
             clientId
         ], (error, rows) => {
-            if(error) throw error;
+            if (error) throw error;
             return res.json(rows);
         })
     }
