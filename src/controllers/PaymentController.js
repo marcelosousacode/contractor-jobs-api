d111ba6609abd1a4ac18029d9fa04925c0f8f268
Marcelo Sousa <marcelosousacode@gmail.com>
2021-09-25 14:10:00 -0300

    Implement Stripe payment

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -1,95 +1,47 @@
-const stripe = require('../configs/stripe');
-const connection = require('../db/connection');
+const stripe = require('../configs/stripe')
 
 module.exports = {
     async createPaymentIntent(req, res) {
         const { items } = req.body;
-
-        try {
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount: items.amount,
-                currency: "brl",
-                payment_method_types: ['card'],
-            });
-
-            res.send({
-                clientSecret: paymentIntent.client_secret,
-                id: paymentIntent.id
-            });
-        } catch (error) {
-            res.send({
-                status: error.statusCode,
-                type: error.type,
-                message: error.raw.message
-            });
-        }
+    
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: items.amount,
+            currency: "brl",
+            payment_method_types: ['card'],
+        });
+    
+        res.send({
+            clientSecret: paymentIntent.client_secret,
+            id: paymentIntent.id
+        });
     },
     async confirmPaymentIntent(req, res) {
         const { paymentIntentId, paymentMethod } = req.body;
-        let confirm;
-
-        try {
-            confirm = await stripe.paymentIntents.confirm(
-                paymentIntentId,
-                { payment_method: paymentMethod }
-            );
-
-            res.send({
-                confirm
-            });
-        } catch (error) {
-            res.send({
-                status: error.statusCode,
-                type: error.type,
-                message: error.raw.message
-            });
-        }
+    
+        const confirm = await stripe.paymentIntents.confirm(
+            paymentIntentId,
+            { payment_method: paymentMethod }
+        );
+    
+        res.send({
+            confirm
+        });
     },
     async createPaymentMethod(req, res) {
         const { type, card } = req.body;
-
-        try {
-            const paymentMethod = await stripe.paymentMethods.create({
-                type: type,
-                card: {
-                    number: card.number,
-                    exp_month: card.exp_month,
-                    exp_year: card.exp_year,
-                    cvc: card.cvc,
-                },
-            });
-
-            res.send({
-                paymentMethod: paymentMethod.id
-            })
-        } catch (error) {
-            res.send({
-                status: error.statusCode,
-                type: error.type,
-                message: error.raw.message
-            });
-        }
-    },
-    async savePayment(req, res) {
-        const { clientId, payment } = req.body;
-
-        await connection.query(`
-            INSERT INTO payment (
-                type_card,
-                number,
-                expiration_at,
-                code,
-                fk_client
-            ) VALUES (?, ?, ?, ?, ?)
-        `, [
-            payment.type,
-            payment.number,
-            payment.expirationAt,
-            payment.code,
-            clientId
-        ], (error, rows) => {
-            if (error) throw error;
-            return res.json(rows);
+        
+        const paymentMethod = await stripe.paymentMethods.create({
+            type: type,
+            card: {
+                number: card.number,
+                exp_month: card.exp_month,
+                exp_year: card.exp_year,
+                cvc: card.cvc,
+            },
+        });
+    
+        res.send({
+            paymentMethod: paymentMethod.id
         })
     }
 }
--- src/controllers/ProfessionalController.js
+++ src/controllers/ProfessionalController.js
@@ -2,20 +2,15 @@ const connection = require('../db/connection');
 
 module.exports = {
     async index(req, res) {
-        await connection.query(`SELECT DISTINCT professional.*, profession.name as name_profession FROM item_professional_profission
-        INNER JOIN professional ON professional.id = item_professional_profission.fk_professional
-        INNER JOIN profession ON profession.id = item_professional_profission.fk_profission;`, (err, rows) => {
+        await connection.query('SELECT * FROM professional', (err, rows) => {
             if (err) throw err
             return res.json(rows)
         })
     },
-
+    
     async show(req, res) {
-        const id = req.params.id;
-        await connection.query(`SELECT DISTINCT professional.*, profession.name as name_profession FROM item_professional_profission
-        INNER JOIN professional ON professional.id = item_professional_profission.fk_professional
-        INNER JOIN profession ON profession.id = item_professional_profission.fk_profission WHERE profession.id = ?;`, [
-            id
+        await connection.query('SELECT * FROM professional WHERE professional.id=?', [
+            req.params.id
         ], (err, rows) => {
             if (err) throw err
             return res.json(rows[0])
