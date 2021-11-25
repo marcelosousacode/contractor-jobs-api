685dfdaf5b23711781fef7a8b6ce5152a772d45f
Marcelo Sousa <marcelosousacode@gmail.com>
2021-11-25 14:10:00 -0300

    add filter scheduling requests by data and add filter professional by rate (#31)

--- src/controllers/ProfessionalProfessionController.js
+++ src/controllers/ProfessionalProfessionController.js
@@ -4,7 +4,7 @@ module.exports = {
     async index(req, res) {
         await connection.query(`SELECT DISTINCT professional.*, profession.name as name_profession FROM item_professional_profession
         INNER JOIN professional ON professional.id = item_professional_profession.fk_professional
-        INNER JOIN profession ON profession.id = item_professional_profession.fk_profession;`, (err, rows) => {
+        INNER JOIN profession ON profession.id = item_professional_profession.fk_profession ORDER BY rate DESC;`, (err, rows) => {
             if(err) throw err
             return res.json(rows)
         })
@@ -13,7 +13,7 @@ module.exports = {
     async selectProfessionalsByProfession(req, res) {
         await connection.query(`SELECT DISTINCT professional.*, profession.name as name_profession, profession.id as profession_id FROM item_professional_profession
         INNER JOIN professional ON professional.id = item_professional_profession.fk_professional
-        INNER JOIN profession ON profession.id = item_professional_profession.fk_profession WHERE profession.id = ?;`, [
+        INNER JOIN profession ON profession.id = item_professional_profession.fk_profession WHERE profession.id = ? ORDER BY rate DESC;`, [
             req.params.id
         ], (err, rows) => {
             if (err) throw err
--- src/controllers/SchedulingClientController.js
+++ src/controllers/SchedulingClientController.js
@@ -7,7 +7,7 @@ module.exports = {
         const id = req.params.id;
         await connection.query(`SELECT scheduling.*, client.name, client.email, client.phone_number, client.uf, client.city, client.email FROM scheduling
             INNER JOIN client ON client.id = scheduling.fk_client
-            WHERE scheduling.fk_client=?`, [
+            WHERE scheduling.fk_client=? ORDER BY scheduling.end ASC;`, [
                 id
             ],
         (err, rows) => {
@@ -22,12 +22,11 @@ module.exports = {
         await connection.query(`SELECT DISTINCT client.name, client.email, client.phone_number, client.uf, client.city, scheduling.id, scheduling.title, scheduling.description, scheduling.start, scheduling.end FROM scheduling
         INNER JOIN professional ON professional.id = scheduling.fk_professional
         INNER JOIN client ON client.id = scheduling.fk_client WHERE professional.id = ? AND scheduling.status="PENDENTE"
-        ORDER BY scheduling.date DESC;`, [
+        ORDER BY scheduling.end ASC;`, [
             idProfessionalLogged
         ], (err, rows) => {
             if (err) throw err
             return res.json(rows)
         })
     }
-
 }
